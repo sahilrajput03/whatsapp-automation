0189333d7fd7879b0da0f2615414430395ed98e2
Sahil Rajput <sahilrajput03@gmail.com>
2025-09-12 20:43:16 +0530

    Update.

--- wwebclient.js
+++ wwebclient.js
@@ -6,11 +6,14 @@ const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 // By Default it uses a persistent storage to store the session, yikes!
 // const client = new Client({ authStrategy: new LocalAuth(), });
 
+// const clientid = 'sahil' // unique ID per number 
+const clientid = 'topfivebestrated'; // unique ID per number 
+// ! Add a check before commiting via `git commit hook` so that I never commit without clientid as `topfivebestrated`
+
 const client = new Client({
     authStrategy: new LocalAuth({
         // * Note: 'sahil' for my personal and 'topfivebestrated' for himanshu's bot.
-        // clientId: 'sahil', // unique ID per number 
-        clientId: 'topfivebestrated', // unique ID per number
+        clientId: clientid,
         // Learn: Default `dataPath` folder path is `.wwebjs_auth`
         // dataPath: './sessions' // base folder for all accounts
     })
